3f73548f69105bb82560ec6565c801d8f4f226fe
aliceyang <aliceyang@berkeley.edu>
2018-03-01 15:15:23 -0800

    hw5

--- app/static/interactions.js
+++ app/static/interactions.js
@@ -1,9 +1,24 @@
-$('#submit-survey').on('click', function submitSurvey() {
+$("#submit-survey").on("click", function submitSurvey() {
+	// console.log(test);
 	var color = $("input[name=color]").val();
 	var food = $("input[name=food]").val();
 	var vacation = $("input[name=vacation]").val();
 	var feBefore = $("input[name=front-end-before]").val();
 	var feAfter = $("input[name=front-end-after]").val();
+
+	// console.log(test);
+
+	$.post("/submit-survey",
+		{ "color": color,
+		"food": food,
+		"vacation": vacation,
+		"feBefore": feBefore,
+		"feAfter": feAfter },
+		function(user-input) {
+			// console.log(test);
+			$(.result-item).html(user-input);
+		});
+        
 });
 
 $("#site-title-wrapper").on('click', function goHome() {
--- app/views.py
+++ app/views.py
@@ -7,19 +7,24 @@ app.secret_key = os.environ.get('SECRET_KEY') or 'hard to guess string'
 @app.route('/')
 @app.route('/index')
 def index():
-    username = ''
-    if (): #check if the user is already in session, if so, direct the user to survey.html Hint: render_template with a variable
-        pass
+    if 'username' in session:        
+        return render_template('survey.html', username=session['username'])
     else:
         return render_template('login.html')
 
-@app.route('/login') # You need to specify something here for the function to get requests
+@app.route('/login', methods=['GET', 'POST']) # You need to specify something here for the function to get requests
 def login():
     # Here, you need to have logic like if there's a post request method, store the username and email from the form into
     # session dictionary
-    if():
-        pass
-    return None
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        session['email'] = request.form['email']
+    return redirect(url_for('index'))
+    
+
+    # if():
+    #     pass
+    # return None
 
 @app.route('/logout')
 def logout():
@@ -29,16 +34,24 @@ def logout():
 
 @app.route('/submit-survey', methods=['GET', 'POST'])
 def submitSurvey():
-    username = ''
-    email = ''
-    if(): #check if user in session
-        username = session.get('username')
+    # print(123)
+    # username = ''
+    # email = ''
+    # if(): #check if user in session
+    if 'username' in session:
+        # username = session.get('username')
+        # print(123)
+        # username = session['username']
         surveyResponse = {}
         #get the rest o responses from users using request library Hint: ~3 lines of code
-        surveyResponse['fe-before'] = request.form.get('feBefore')
-        surveyResponse['fe-after'] = request.form.get('feAfter')
-        return render_template('results.html') # pass in variables to the template
+        surveyResponse['color'] = request.form.get('color')
+        surveyResponse['food'] = request.form.get('food')
+        surveyResponse['vacation'] = request.form.get('vacation')
+        surveyResponse['feBefore'] = request.form.get('feBefore')
+        surveyResponse['feAfter'] = request.form.get('feAfter')
+        return render_template('results.html', username=session['username'], surveyResponse=surveyResponse) # pass in variables to the template
     else:
+        # print(123)
         return render_template('login.html')
 
 @app.errorhandler(404)
